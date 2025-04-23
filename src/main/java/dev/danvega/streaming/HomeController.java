bfc4d0ba8430fc91b3e4dc774ad5627e810595cd
subash770 <spidybeatz25@gmail.com>
2025-04-23 11:36:38 +0530

    add home-controller

---
+++ src/main/java/dev/danvega/streaming/HomeController.java
@@ -0,0 +1,14 @@
+package dev.danvega.streaming;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "stream"; // refers to templates/stream.html
+    }
+}
+
